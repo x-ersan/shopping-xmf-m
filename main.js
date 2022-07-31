9f0a6ecaf69a1f15fade865fbe277c83bc4d898c
ersan <2324164705@qq.com>
2022-08-01 00:06:46 +0800

    完成首页开发

--- main.js
+++ main.js
@@ -2,7 +2,31 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
 
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+// 基地址
+$http.baseUrl = 'https://www.uinav.com'
+// 请求拦截器
+$http.beforeRequest=function(option){
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+// 响应拦截器
+$http.afterRequest=function(){
+  uni.hideLoading()
+}
+// 封装消息提示方法
+uni.$showMsg =function(title='数据加载中', duration = 1500){
+  uni.showToast({
+    title,
+    duration,
+    icon:'none'
+  })
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
